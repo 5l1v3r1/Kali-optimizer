e60a3279f6d43548dcaccc02c42c0c7244e7f4ac
Haroon Awan <39236904+haroonawanofficial@users.noreply.github.com>
2019-04-01 14:10:12 +0500

    Update kali_optimizer.sh

--- kali_optimizer.sh
+++ kali_optimizer.sh
@@ -18,26 +18,6 @@
 #     information.                                                                        #
 #  8. It can be used to take measures achieve protection.                                 #
 # *************************************************************************************** #
-#!/bin/bash
-# *************************************************************************************** #
-# ---------------------------------- EULA NOTICE ---------------------------------------- #
-#                     Agreement between "Haroon Awan" and "You"(user).                    #
-# ---------------------------------- EULA NOTICE ---------------------------------------- #
-#  1. By using this piece of software your bound to these point.                          #
-#  2. This an End User License Agreement (EULA) is a legal between a software application #
-#     author "Haroon Awan" and (YOU) user of this software.                               #
-#  3. This software application grants users rights to use for any purpose or modify and  #
-#     redistribute creative works.                                                        #
-#  4. This software comes in "is-as" warranty, author "Haroon Awan" take no responsbility #
-#     what you do with by/this software as your free to use this software.                #
-#  5. Any other purpose(s) that it suites as long as it is not related to any kind of     #
-#     crime or using it in un-authorized environment.                                     #
-#  6. You can use this software to protect and secure your data information in any        #
-#     environment.                                                                        #
-#  7. It can also be used in state of being protection against the unauthorized use of    #
-#     information.                                                                        #
-#  8. It can be used to take measures achieve protection.                                 #
-# *************************************************************************************** #
 
 OLDCONF=$(dpkg -l|grep "^rc"|awk '{print $2}')
 CURKERNEL=$(uname -r|sed 's/-*[a-z]//g'|sed 's/-386//g')
@@ -57,6 +37,10 @@ echo -e $YELLOW"[ + ] Clearing cache and trash "$ENDCOLOR
 sudo rm -r /etc/apt/sources.list.d/*
 rm -rf /home/*/.local/share/Trash/*/** &> /dev/null
 rm -rf /root/.local/share/Trash/*/** &> /dev/null
+echo -e 
+echo -e $YELLOW"[ + ] Writing proper sources list "$ENDCOLOR
+echo -e "y" | cp /etc/apt/sources.list /etc/apt/sources.list.backup
+echo -e "y" | cp /root/Downloads/kali_optimizer-master/sources.list /etc/apt/
 echo -e 
 echo -e $YELLOW"[ + ] Updating kali "$ENDCOLOR
 echo -e "y" | sudo apt autoremove
